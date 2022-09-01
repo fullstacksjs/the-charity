e619675e6389ce10f247f9c19c9edac1ff574ebb
erfangoodazi <63591256+erfan-goodarzi@users.noreply.github.com>
2022-09-01 12:15:18 +0430

    add mantine ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Title } from '@mantine/core';
 import { ReactLocation, Router } from '@tanstack/react-location';
 
 const location = new ReactLocation();
@@ -5,7 +6,7 @@ const location = new ReactLocation();
 export const App: React.FC = () => {
   return (
     <Router routes={[{ path: '/' }]} location={location}>
-      The Charity App
+      <Title order={1}>The Charity App</Title>
     </Router>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
+import { MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <MantineProvider withGlobalStyles withCSSVariables>
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
 );
