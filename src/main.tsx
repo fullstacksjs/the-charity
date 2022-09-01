273cb5a1d33aa477c93d833498558a6ae4b7a436
erfangoodazi <63591256+erfan-goodarzi@users.noreply.github.com>
2022-09-01 12:19:06 +0430

    add RTL support

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
-import { MantineProvider } from '@mantine/core';
 import React from 'react';
+import { MantineProvider, createEmotionCache } from '@mantine/core';
+import rtlPlugin from 'stylis-plugin-rtl';
 import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
+
+const rtlCache = createEmotionCache({
+  key: 'mantine-rtl',
+  stylisPlugins: [rtlPlugin],
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,7 +16,11 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <MantineProvider withGlobalStyles withCSSVariables>
+    <MantineProvider
+      withGlobalStyles
+      withCSSVariables
+      emotionCache={rtlCache}
+      theme={{ dir: 'rtl' }}>
       <App />
     </MantineProvider>
   </React.StrictMode>
