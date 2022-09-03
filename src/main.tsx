b66a15b17b64f6c29988e5cc89590883120ac915
Erfan Goodarzi <63591256+erfan-goodarzi@users.noreply.github.com>
2022-09-04 00:29:27 +0430

    [FKS-15] : Configure UI framework (#4)
    Install and configure Mantine
    
    Co-authored-by: AmirabbasJ <amirabbasgamer2@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
+import { createEmotionCache, MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import rtlPlugin from 'stylis-plugin-rtl';
 
 import { App } from './App';
+
+const rtlCache = createEmotionCache({
+  key: 'mantine-rtl',
+  stylisPlugins: [rtlPlugin],
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -9,6 +16,16 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider
+      withGlobalStyles
+      withCSSVariables
+      emotionCache={rtlCache}
+      theme={{
+        dir: 'rtl',
+        fontFamily: 'Iranian Sans, sans-serif',
+      }}
+    >
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 );
