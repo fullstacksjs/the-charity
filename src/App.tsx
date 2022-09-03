dd92a746ce05d986a4d5402445b9b162a8b0f7e8
Alireza Safaierad <frontendmonster@gmail.com>
2022-09-04 00:31:21 +0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const location = new ReactLocation();
 export const App: React.FC = () => {
   return (
     <Router routes={[{ path: '/' }]} location={location}>
-      <Title order={1}>The Charity App</Title>
+      <Title order={1}>The Charity App Test</Title>
     </Router>
   );
 };
