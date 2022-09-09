2d38cab4210126b6963f9085471dbb8567e5749d
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-09-10 01:40:31 +0430

    fix eslint warning

--- src/config/getEnv.ts
+++ src/config/getEnv.ts
@@ -2,5 +2,7 @@ import type { Env } from '../domain';
 
 // TODO: add parsing
 export const getEnv = (): Env => {
+  /* eslint-disable @typescript-eslint/naming-convention */
   return { APP_GRAPHQL_SCHEMA_URL: import.meta.env.APP_GRAPHQL_SCHEMA_URL };
+  /* eslint-enable @typescript-eslint/naming-convention */
 };
--- src/domain/Env.ts
+++ src/domain/Env.ts
@@ -1,3 +1,5 @@
 export interface Env {
+  /* eslint-disable @typescript-eslint/naming-convention */
   APP_GRAPHQL_SCHEMA_URL: string;
+  /* eslint-enable @typescript-eslint/naming-convention */
 }
