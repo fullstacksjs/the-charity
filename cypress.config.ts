3a3e2083d40da5c2237f8ee05ead7db2276e20d7
Erfan Goodarzi <63591256+erfan-goodarzi@users.noreply.github.com>
2022-09-09 15:51:19 +0430

    [FSK-16]: Configure Cypress (#13)
    This PR adds cypress configuration.

---
+++ cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://127.0.0.1:5173',
+    projectId: 'dpxh3q',
+  },
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+    },
+  },
+});
