c746cbac94d2fb9ee613e2be0dd0cc875759a6b2
Alireza Safaierad <frontendmonster@gmail.com>
2022-09-10 18:27:21 +0400

    [Hotfix]: Improve Configurations (#18)
    this PR improves configurations by updating the linter package and README

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://127.0.0.1:5173',
-    projectId: 'dpxh3q',
+    projectId: '8jt3ix',
   },
   component: {
     devServer: {
