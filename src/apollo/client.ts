a755d89751fd7a74a59e3ab864e3679b041c689f
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-09-11 19:09:28 +0430

    add simple apollo error handling

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,10 +1,23 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, from, HttpLink, InMemoryCache } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
 
 import { getConfig } from '../config';
 
 const config = getConfig();
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const httpLink = new HttpLink({ uri: config.schemaUrl });
+
 export const client = new ApolloClient({
-  uri: config.schemaUrl,
+  link: from([errorLink, httpLink]),
   cache: new InMemoryCache(),
 });
