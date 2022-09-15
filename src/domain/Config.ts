0e6efb2fa33d50af20ea475625862f0681b0678b
Alireza Safaierad <frontendmonster@gmail.com>
2022-09-15 12:03:52 +0400

    add Path schema

--- src/domain/Config.ts
+++ src/domain/Config.ts
@@ -1,10 +1,11 @@
 import type { Static } from 'runtypes';
-import { Record } from 'runtypes';
+import { Record, Union } from 'runtypes';
 
+import { Path } from './Path';
 import { Url } from './Url';
 
 export const Config = Record({
-  schemaUrl: Url,
+  schemaUrl: Union(Url, Path),
 });
 
 export type Config = Static<typeof Config>;
