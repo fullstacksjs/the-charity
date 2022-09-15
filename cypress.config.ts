bd9388f4347628448ffa27839fc2ba835d4f5d49
Alireza Safaierad <frontendmonster@gmail.com>
2022-09-15 10:35:22 +0400

    read port from env for cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,10 @@
+import { getRequiredEnv, toInteger } from '@fullstacksjs/toolbox';
 import { defineConfig } from 'cypress';
 
+const port = toInteger(getRequiredEnv('PORT'), 3000);
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:5173',
+    baseUrl: `http://127.0.0.1:${port}`,
     projectId: '8jt3ix',
   },
   component: {
