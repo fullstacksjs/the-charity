85cff2eac22cd7d453683dd5f0baefbfb6dbb71e
mojtaba eslami <mojtabaeslami.72@gmail.com>
2022-09-16 16:10:42 +0430

    Convert apollo.config to a javascript file

---
+++ apollo.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  service: {
+    name: 'the-charity-frontend',
+    url: 'https://the-charity-backend-staging.herokuapp.com/graphql',
+  },
+  includes: ['./src/operations/**/*.graphql', './src/operations/**/*.ts'],
+  excludes: ['./src/operations/__generated/*.ts'],
+};
