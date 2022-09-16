9c753e26a897ce3816e3b7493a57336369ddce2f
mojtaba eslami <mojtabaeslami.72@gmail.com>
2022-09-16 18:12:47 +0430

    Fix accidental deleted lines in apollo.config.js

--- apollo.config.js
+++ apollo.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  service: {
-    name: 'the-charity-frontend',
-    url: 'https://the-charity-backend-staging.herokuapp.com/graphql',
+  client: {
+    service: {
+      name: 'the-charity-frontend',
+      url: 'https://the-charity-backend-staging.herokuapp.com/graphql',
+    },
+    includes: ['./src/operations/**/*.graphql', './src/operations/**/*.ts'],
+    excludes: ['./src/operations/__generated__/*.ts'],
   },
-  includes: ['./src/operations/**/*.graphql', './src/operations/**/*.ts'],
-  excludes: ['./src/operations/__generated__/*.ts'],
 };
