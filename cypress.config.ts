d30c89ef719bfb1c97f645dd55d14a9fe7c739c8
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-09-20 23:14:39 +0430

    fix port being required in cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,8 @@
-import { getRequiredEnv, toInteger } from '@fullstacksjs/toolbox';
+import { getEnv, toInteger } from '@fullstacksjs/toolbox';
 import { defineConfig } from 'cypress';
 
-const port = toInteger(getRequiredEnv('PORT'), 3000);
+const port = toInteger(getEnv('PORT', ''), 3000);
+
 export default defineConfig({
   e2e: {
     baseUrl: `http://127.0.0.1:${port}`,
