9c43163bbeebf51e93e024fe4bd3a9c1d1b0f8aa
erfangoodazi <63591256+erfan-goodarzi@users.noreply.github.com>
2022-09-21 10:40:35 +0430

    create navigation links and add icons

---
+++ src/components/appShell/atoms/Logo.tsx
@@ -0,0 +1,11 @@
+import MainLogo from '../../../assets/image/main-logo.svg';
+
+const Logo = () => {
+  return (
+    <>
+      <img src={MainLogo} alt="logo" />
+    </>
+  );
+};
+
+export default Logo;
