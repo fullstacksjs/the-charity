0620d06b5d8118f804ab6071f0ba4c2478d4cbc8
erfangoodazi <63591256+erfan-goodarzi@users.noreply.github.com>
2022-09-21 11:33:25 +0430

    create empty state

--- src/components/appShell/AppShell.tsx
+++ src/components/appShell/AppShell.tsx
@@ -1,4 +1,6 @@
 import Logo from './atoms/Logo';
+import PeopleIcon from './icons/components/PeopleIcon';
+import EmptyState from './molecules/EmptyState';
 import NavList from './molecules/NavList';
 
 const AppShell = () => {
@@ -6,6 +8,11 @@ const AppShell = () => {
     <>
       <Logo />
       <NavList />
+      <EmptyState
+        icon={<PeopleIcon w="33" h="33" />}
+        title="پروژه ای وجود ندارد!"
+        message="متاسفانه لیست پروژه های شما خالی است. لطفا پروژه خود را ایجاد کنید."
+      />
     </>
   );
 };
--- src/components/appShell/molecules/EmptyState.tsx
+++ src/components/appShell/molecules/EmptyState.tsx
@@ -1,5 +1,24 @@
-const EmptyState = () => {
-  return <div>EmptyState</div>;
+import { Space, Text } from '@mantine/core';
+import type { ReactNode } from 'react';
+
+import VisualElements from '../atoms/VisualElements';
+
+interface EmptyStateProps {
+  icon: ReactNode;
+  title: string;
+  message: string;
+}
+
+const EmptyState = ({ icon, title, message }: EmptyStateProps) => {
+  return (
+    <>
+      <VisualElements icon={icon} />
+      <Space h="md" />
+      <Text weight={700}>{title}</Text>
+      <Space h="xs" />
+      <Text color="#495057">{message}</Text>
+    </>
+  );
 };
 
 export default EmptyState;
