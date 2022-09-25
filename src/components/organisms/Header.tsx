d67dcec983c7703d0112af50b316fbfb43556fe8
erfangoodazi <63591256+erfan-goodarzi@users.noreply.github.com>
2022-09-25 14:37:24 +0330

    remove unnecessary component and test

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,4 +1,4 @@
-import { Button, Col, Grid } from '@mantine/core';
+import { Col, Grid } from '@mantine/core';
 
 import { HomeIcon } from '../appShell/icons/Home';
 
@@ -8,11 +8,7 @@ export const Header = () => {
       <Col span={10}>
         <HomeIcon w="25" h="25" />
       </Col>
-      <Col span={2}>
-        <Button variant="outline" size="sm">
-          ایجاد پروژه جدید
-        </Button>
-      </Col>
+      <Col span={2} />
     </Grid>
   );
 };
