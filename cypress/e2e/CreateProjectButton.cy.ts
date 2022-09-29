36f8bff6c581c7e882216a64e7061050814d2c6f
erfangoodazi <63591256+erfan-goodarzi@users.noreply.github.com>
2022-09-29 13:29:03 +0330

    improve CreateProjectButton e2e test

--- cypress/e2e/CreateProjectButton.cy.ts
+++ cypress/e2e/CreateProjectButton.cy.ts
@@ -1,6 +1,6 @@
 describe('Create Project Button', () => {
   it('Finds the Create Project Button in projects page', () => {
     cy.visit('/projects');
-    cy.get('[data-test-id="create-project"]');
+    cy.get('[data-test-id="create-project"]').should('exist');
   });
 });
