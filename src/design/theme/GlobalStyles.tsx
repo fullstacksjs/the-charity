f68f71113394d67c3d8834f5064d8aa5414dc580
mojtaba eslami <mojtabaeslami.72@gmail.com>
2022-10-01 02:01:37 +0330

    Add global reset

--- src/design/theme/GlobalStyles.tsx
+++ src/design/theme/GlobalStyles.tsx
@@ -6,10 +6,22 @@ export const GlobalStyles = () => {
       styles={[
         /* eslint-disable @typescript-eslint/naming-convention */
         {
+          /* 1.Use a more-intuitive box-sizing model. */
+          '*, *::before, *::after': {
+            boxSizing: 'border-box',
+          },
+          /* 2.Remove default margin */
           '*': {
             margin: 0,
-            padding: 0,
-            boxSizing: 'border-box',
+          },
+          /* 3.Allow percentage-based heights in the application */
+          'html, body': {
+            height: '100%',
+          },
+          /* 4.Improve media defaults */
+          'img, picture, video, canvas, svg': {
+            display: 'block',
+            maxWidth: '100%',
           },
         },
         /* eslint-enable @typescript-eslint/naming-convention */
