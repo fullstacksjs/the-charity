340206bb413062a1bf1a53e436ba334348b56cd8
mojtaba eslami <mojtabaeslami.72@gmail.com>
2022-10-01 17:57:30 +0330

    Fix Navlink style issues

--- src/components/atoms/NavLink/NavLink.tsx
+++ src/components/atoms/NavLink/NavLink.tsx
@@ -1,36 +1,39 @@
-import { NavLink as Link } from '@mantine/core';
+import { createStyles, NavLink as MantineNavLink } from '@mantine/core';
 import { Link as TanstackLink, useLocation } from '@tanstack/react-location';
 import type { ReactNode } from 'react';
+import React from 'react';
 
 export interface NavLinkProps {
   label: string;
   path: string;
   icon?: ReactNode;
 }
+const useStyles = createStyles(theme => ({
+  root: {
+    borderRadius: 4,
+    fontWeight: 500,
+    padding: 10,
+    color: theme.colors.fgMuted[6],
+  },
+}));
 
-export const NavLink = ({ label, icon, path }: NavLinkProps) => {
+export const NavLink: React.FC<NavLinkProps> = ({ label, icon, path }) => {
+  const { classes } = useStyles();
   const {
     current: { pathname },
   } = useLocation();
 
   return (
-    <Link
+    <MantineNavLink
       to={path}
       component={TanstackLink}
       data-test-id="nav-link"
-      color="indigo"
       key={label}
-      sx={{
-        borderRadius: '4px',
-        fontWeight: 500,
-        padding: 10,
-        margin: '15px 0',
-        width: 240,
-        height: 44,
-      }}
       label={label}
       rightSection={icon}
       active={pathname === path}
+      color="indigo"
+      className={classes.root}
     />
   );
 };
