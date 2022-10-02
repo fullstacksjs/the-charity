17c726302fe45e310c309b60e20d7c20abf63471
mojtaba eslami <mojtabaeslami.72@gmail.com>
2022-10-02 04:47:23 +0330

    Add onClick handler to create Project button

--- src/components/CreateProjectButton/CreateProjectButton.tsx
+++ src/components/CreateProjectButton/CreateProjectButton.tsx
@@ -2,7 +2,11 @@ import { PlusIcon } from '@camp/design';
 import { Button } from '@mantine/core';
 import { Link } from '@tanstack/react-location';
 
-export const CreateProjectButton = () => {
+interface Props {
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export const CreateProjectButton = ({ onClick }: Props): JSX.Element => {
   return (
     <Link to="/create-project">
       <Button
@@ -18,6 +22,7 @@ export const CreateProjectButton = () => {
         size="sm"
         id="create-project"
         leftIcon={<PlusIcon width="16" height="16" />}
+        onClick={onClick}
       >
         ایجاد پروژه جدید
       </Button>
