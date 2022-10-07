58a3f9bd95deb9b607820124c02f28a173a06d4f
mojtaba eslami <mojtabaeslami.72@gmail.com>
2022-10-07 16:30:58 +0330

    Separate CreateFamily form and modal tests

---
+++ src/components/CreateFamily/FamilyForm/CreateFamilyForm.cy.tsx
@@ -0,0 +1,63 @@
+import { noop } from '@fullstacksjs/toolbox';
+
+import { CreateFamilyForm, createFamilyFormIDs } from './CreateFamilyForm';
+
+const requiredFieldMessage = 'این فیلد ضروری است';
+const minLengthMessage = 'نام خانواده باید حداقل ۳ حرف باشد';
+
+describe('Create Family Form', () => {
+  beforeEach(() => {
+    cy.mount(<CreateFamilyForm dismiss={noop} />);
+  });
+
+  it('should contains text input to enter family name', () => {
+    cy.get('form').within(() => {
+      cy.findByTestId(createFamilyFormIDs.nameInput).should('match', 'input');
+    });
+  });
+
+  it('should contains submit button to create family', () => {
+    cy.get('form').within(() => {
+      cy.findByTestId(createFamilyFormIDs.submitBtn)
+        .should('have.attr', 'type')
+        .and('equal', 'submit');
+    });
+  });
+
+  it('should not show a required error message when family name is not empty', () => {
+    cy.get('form').within(() => {
+      cy.findByTestId(createFamilyFormIDs.nameInput).type('مرادی');
+      cy.root().submit();
+      cy.findByText(`/${requiredFieldMessage}/`).should('not.exist');
+    });
+  });
+
+  it('should show a required error message when family name is empty', () => {
+    cy.get('form').within(() => {
+      cy.root().submit();
+      cy.findByRole('alert').should('have.text', requiredFieldMessage);
+    });
+  });
+
+  it('should show an error message when family name is less than min length', () => {
+    cy.get('form').within(() => {
+      cy.findByTestId(createFamilyFormIDs.nameInput).type('م');
+      cy.findByRole('alert').should('have.text', minLengthMessage);
+    });
+  });
+
+  it('should not show an error message when family name is more than or equal min length', () => {
+    cy.get('form').within(() => {
+      cy.findByTestId(createFamilyFormIDs.nameInput).type('مرادی');
+      cy.findByText(`/${minLengthMessage}/`).should('not.exist');
+    });
+  });
+
+  it('should submit when all required fields fill correctly', () => {
+    cy.get('form').within(() => {
+      cy.findByTestId(createFamilyFormIDs.nameInput).type('مرادی');
+      cy.findByText(`/${minLengthMessage}/`).should('not.exist');
+      cy.root().submit();
+    });
+  });
+});
