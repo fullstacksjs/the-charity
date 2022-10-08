379669bcfbed6a1edb3bf1de95713ce81842274a
Alireza Safaierad <frontendmonster@gmail.com>
2022-10-08 21:33:29 +0400

    make route type safe

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import type {
+  MakeGenerics,
+  Route as LocationRoute,
+} from '@tanstack/react-location';
 import { Navigate, ReactLocation, Router } from '@tanstack/react-location';
 
 import { AppShell } from './components';
@@ -5,28 +10,38 @@ import { Families, Projects } from './pages';
 
 const location = new ReactLocation();
 
+type LocationGenerics = MakeGenerics<{
+  RouteMeta: {
+    breadcrumb: string;
+  };
+}>;
+
+interface Route extends Omit<LocationRoute<LocationGenerics>, 'path'> {
+  path?: AppRoute;
+  children?: Route[];
+}
+
+const routes: Route[] = [
+  {
+    path: '/families',
+    element: <Families />,
+    meta: {
+      breadcrumb: 'خانواده ها',
+    },
+  },
+  {
+    path: '/projects',
+    element: <Projects />,
+    meta: {
+      breadcrumb: 'پروژه ها',
+    },
+  },
+  { element: <Navigate to="/families" /> },
+];
+
 export const Routes = () => {
   return (
-    <Router
-      routes={[
-        { path: '/', element: <Navigate to="/families" /> },
-        {
-          path: '/families',
-          element: <Families />,
-          meta: {
-            breadcrumb: () => 'خانواده ها',
-          },
-        },
-        {
-          path: '/projects',
-          element: <Projects />,
-          meta: {
-            breadcrumb: () => 'پروژه ها',
-          },
-        },
-      ]}
-      location={location}
-    >
+    <Router routes={routes} location={location}>
       <AppShell />
     </Router>
   );
