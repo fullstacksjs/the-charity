f6051ee52238663ac4021eaf3a27c883cc5bc599
mojtaba eslami <mojtabaeslami.72@gmail.com>
2022-10-16 17:35:16 +0330

    Add breadcrumbs hook to header

--- src/components/molecules/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/molecules/Breadcrumbs/Breadcrumbs.tsx
@@ -11,26 +11,42 @@ interface Props {
   items: BreadcrumbItem[];
 }
 
+// const concatPathLevels = (items: BreadcrumbItem[]) => {
+//   // return items.map((item, level) => {
+//   //   if (level >= 1) {
+//   //     // item.path = localItems[level - 1]?.path.concat(item.path) ?? '';
+//   //   }
+//   return items.map((item, level) => {
+//     if (level === 0) return item;
+//     return items.reduce((acc, item, index) => {
+//       console.log(index);
+
+//       if (index <= level) {
+//         return {
+//           ...item,
+//           path: acc.path + item.path,
+//         };
+//       }
+//     });
+//   });
+// };
+
 export const Breadcrumbs = ({ items }: Props) => {
+  // console.log('items', items);
+  // const transformedItems = concatPathLevels(items);
+  // console.log('transformed', transformedItems);
   return (
     <MantineBreadcrumbs separator={<ChevronLeftIcon width="16" height="16" />}>
       <HomeIcon width="24" height="24" />
-      {items.map((item, level) => {
-        const composedPath = items.reduce((acc, i, index) => {
-          if (index <= level) return `${acc}/${i.path}`;
-          return acc;
-        }, '') as any;
-
-        return (
-          <Anchor
-            key={item.name + item.path}
-            to={composedPath}
-            component={Link}
-          >
-            {item.name}
-          </Anchor>
-        );
-      })}
+      {items.map(item => (
+        <Anchor
+          key={item.name + item.path}
+          to={item.path as any}
+          component={Link}
+        >
+          {item.name}
+        </Anchor>
+      ))}
     </MantineBreadcrumbs>
   );
 };
--- src/components/organisms/Header/DashboardHeader.tsx
+++ src/components/organisms/Header/DashboardHeader.tsx
@@ -1,22 +1,34 @@
 import { Group } from '@mantine/core';
 import { useMatches } from '@tanstack/react-location';
 
-import type { LocationGenerics } from '../../../Routes';
 import { Breadcrumbs } from '../../molecules';
 
 export interface HeaderProps {
   button: React.ReactNode;
 }
 
+export const useCreateBreadcrumbsItems = () => {
+  const matches = useMatches();
+
+  const items = matches.map(match => {
+    const { path, meta } = match.route;
+    return {
+      path: path ?? '',
+      name: meta?.['breadcrumbs'] as string,
+    };
+  });
+
+  return {
+    items,
+  };
+};
+
 export const DashboardHeader = ({ button }: HeaderProps) => {
-  const matches = useMatches<LocationGenerics>();
-  const breadcrumbsName = matches.reverse()[0]?.route?.meta?.breadcrumb;
+  const { items } = useCreateBreadcrumbsItems();
 
   return (
-    <Group position="apart" mb={40}>
-      {breadcrumbsName ? (
-        <Breadcrumbs items={[{ name: breadcrumbsName, path: '/' }]} />
-      ) : null}
+    <Group position="apart">
+      <Breadcrumbs items={items} />
       {button}
     </Group>
   );
