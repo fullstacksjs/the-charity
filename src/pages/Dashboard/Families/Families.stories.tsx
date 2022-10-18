64bd73922414ad786fda480437b17381218798e4
mojtaba eslami <mojtabaeslami.72@gmail.com>
2022-10-18 16:39:37 +0330

    Add story for families page

---
+++ src/pages/Dashboard/Families/Families.stories.tsx
@@ -0,0 +1,22 @@
+import { messages } from '@camp/messages';
+import type { Meta, Story } from '@storybook/react';
+
+import { DashboardLayout } from '../DashboardLayout';
+import { Families } from './Families';
+
+export default {
+  component: Families,
+  args: {
+    router: {
+      route: '/families',
+      layout: DashboardLayout,
+      meta: {
+        breadcrumb: messages.families.title,
+      },
+    },
+  },
+} as Meta;
+
+const Template: Story = () => <Families />;
+
+export const Default: Story = Template.bind({});
