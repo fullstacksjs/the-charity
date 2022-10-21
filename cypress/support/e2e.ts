c95e4babdc79730d54e39de75bebb2861d5d1df9
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-10-21 21:07:15 +0330

    add cypress login command

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,1 +1,13 @@
 import './commands';
+
+const userNameInputSelector = '[type="email"]';
+const passwordInputSelector = '[type="password"]';
+
+Cypress.Commands.add('login', () => {
+  cy.visit('/login');
+  cy.get('form').within(() => {
+    cy.get(userNameInputSelector).type('you@email.com');
+    cy.get(passwordInputSelector).type('password');
+    cy.root().submit();
+  });
+});
