1a7195497fb38f67fa711dfa6e3a701edf07c6d2
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-10-21 20:56:07 +0330

    put global commands in index file

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,1 +1,12 @@
 import '@testing-library/cypress/add-commands';
+
+import type { mount } from 'cypress/react18';
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      mount: typeof mount;
+      login: () => void;
+    }
+  }
+}
