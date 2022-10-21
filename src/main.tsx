9436aeaf1723f868ddb57373b6fa33bc9aac40bb
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-10-21 10:21:56 +0330

    add fake Auth

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,20 @@ import { Routes } from './Routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
+// if you click on "A" (shift+a) you get authenticated
+// if you click on "N" (shift+n) you get unauthenticated
+const fakeAuth = () => {
+  document.addEventListener('keydown', event => {
+    if (event.key === 'A') {
+      const date = new Date();
+      date.setTime(date.getTime() + 100 * 60 * 60 * 1000);
+      document.cookie = `auth=true; expires=${date.toUTCString()}`;
+    } else if (event.key === 'N') document.cookie = 'auth=false;';
+  });
+};
+
+fakeAuth();
+
 root.render(
   <StrictMode>
     <ApolloProvider>
