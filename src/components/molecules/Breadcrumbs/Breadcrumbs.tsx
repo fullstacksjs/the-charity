507f785c24e5e5ffa69ba134501553186d7299b5
mojtaba eslami <mojtabaeslami.72@gmail.com>
2022-10-22 19:10:34 +0330

    Add styles

--- src/components/molecules/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/molecules/Breadcrumbs/Breadcrumbs.tsx
@@ -1,5 +1,6 @@
 import { ChevronLeftIcon, HomeIcon } from '@camp/design';
 import { Link } from '@camp/router';
+import type { Styles } from '@mantine/core';
 import { Anchor, Breadcrumbs as MantineBreadcrumbs } from '@mantine/core';
 
 export interface BreadcrumbItem {
@@ -10,6 +11,18 @@ export interface BreadcrumbItem {
 interface Props {
   items: BreadcrumbItem[];
 }
+
+const styles: Styles<
+  'breadcrumb' | 'root' | 'separator',
+  Record<string, any>
+> = theme => ({
+  breadcrumb: {
+    'color': theme.colors.fgMuted[6],
+    '&:hover': {
+      color: theme.colors.indigo,
+    },
+  },
+});
 
 const concatPathLevels = (items: BreadcrumbItem[]) => {
   const localItems: BreadcrumbItem[] = [];
@@ -27,10 +40,12 @@ const concatPathLevels = (items: BreadcrumbItem[]) => {
 
 export const Breadcrumbs = ({ items }: Props) => {
   const transformedItems = concatPathLevels(items);
-  console.log(transformedItems);
 
   return (
-    <MantineBreadcrumbs separator={<ChevronLeftIcon width="16" height="16" />}>
+    <MantineBreadcrumbs
+      styles={styles}
+      separator={<ChevronLeftIcon width="16" height="16" />}
+    >
       <HomeIcon width="24" height="24" />
       {transformedItems.map(item => (
         <Anchor
