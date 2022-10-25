c1150a920880c34441df685ad9fe3abb1657a963
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-10-25 18:40:01 +0330

    add family empty state

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export * from './atoms';
 export * from './CreateFamily';
 export * from './CreateProject';
 export * from './DashboardCard';
+export * from './FamilyEmptyState';
 export * from './molecules';
 export * from './Notification';
 export * from './organisms';
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
+export * from './Dashboard/DashboardLayout';
 export * from './Dashboard/Families';
 export * from './Dashboard/Projects';
 export * from './Login';
