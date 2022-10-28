358a63302e95ea9e0dd8281552db8646e1b40e1f
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-10-28 23:41:54 +0330

    remove concatPathLevels

--- src/components/molecules/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/molecules/Breadcrumbs/Breadcrumbs.tsx
@@ -24,30 +24,14 @@ const styles: Styles<
   },
 });
 
-const concatPathLevels = (items: BreadcrumbItem[]) => {
-  const localItems: BreadcrumbItem[] = [];
-
-  items.map((item, level) => {
-    if (level === 0) return localItems.push(item);
-    return localItems.push({
-      ...item,
-      path: localItems[level - 1]?.path + item.path,
-    });
-  });
-
-  return localItems;
-};
-
 export const Breadcrumbs = ({ items }: Props) => {
-  const transformedItems = concatPathLevels(items);
-
   return (
     <MantineBreadcrumbs
       styles={styles}
       separator={<ChevronLeftIcon width="16" height="16" />}
     >
       <HomeIcon width="24" height="24" />
-      {transformedItems.map(item => (
+      {items.map(item => (
         <Anchor
           key={item.name + item.path}
           to={item.path as AppRoute}
