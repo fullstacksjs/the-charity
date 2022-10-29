d27e29a015f646d9e7e374e2cc580f382317f564
Alireza Safaierad <frontendmonster@gmail.com>
2022-10-29 20:56:35 +0400

    fix useMatches issue

--- src/components/organisms/Header/DashboardHeader.tsx
+++ src/components/organisms/Header/DashboardHeader.tsx
@@ -1,7 +1,6 @@
+import { useMatches } from '@camp/router';
 import { Group } from '@mantine/core';
-import { useMatches } from '@tanstack/react-location';
 
-import type { LocationGenerics } from '../../../Routes';
 import type { BreadcrumbItem } from '../../molecules';
 import { Breadcrumbs } from '../../molecules';
 
@@ -10,20 +9,17 @@ export interface HeaderProps {
 }
 
 export const useBreadcrumbsItems = (): BreadcrumbItem[] => {
-  const matches = useMatches<LocationGenerics>();
+  const matches = useMatches();
+  console.log(matches);
 
-  return (
-    matches
-      // NOTE: for some reasons react location meta can be undefined in runtime but the type doesn't say that
-      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-      .filter(match => match.route?.meta?.breadcrumb != null)
-      .map(match => {
-        return {
-          path: match.pathname,
-          name: match.route.meta!.breadcrumb,
-        };
-      })
-  );
+  return matches
+    .filter(match => match.route.meta?.breadcrumb != null)
+    .map(match => {
+      return {
+        path: match.pathname,
+        name: match.route.meta!.breadcrumb,
+      };
+    });
 };
 
 export const DashboardHeader = ({ button }: HeaderProps) => {
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,2 +1,3 @@
 export * from './Link';
 export * from './Navigate';
+export * from './useMatches';
