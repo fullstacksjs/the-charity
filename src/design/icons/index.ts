3549954cbb40aa7af8b246d2dc93246e35e1c16a
mojtaba eslami <mojtabaeslami.72@gmail.com>
2022-10-31 00:19:05 +0330

    Add exit icon

--- src/design/icons/index.ts
+++ src/design/icons/index.ts
@@ -1,6 +1,7 @@
 export { ReactComponent as ChevronLeftIcon } from './ChevronLeft.svg';
 export { ReactComponent as DashboardIcon } from './Dashboard.svg';
 export { ReactComponent as ErrorAlertIcon } from './ErrorAlert.svg';
+export { ReactComponent as ExitIcon } from './Exit.svg';
 export { ReactComponent as HomeIcon } from './Home.svg';
 export { ReactComponent as PackageIcon } from './Package.svg';
 export { ReactComponent as PeopleIcon } from './People.svg';
