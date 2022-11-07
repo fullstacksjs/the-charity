afa156c1f139f07a997a0fb2f0d8f11c31b69710
erfangoodazi <63591256+erfan-goodarzi@users.noreply.github.com>
2022-11-07 16:59:45 +0330

    fix await-thenable issue

--- src/data-layer/variables/isAuth.tsx
+++ src/data-layer/variables/isAuth.tsx
@@ -1,12 +1,7 @@
 import { makeVar } from '@apollo/client';
-import {
-  getClientCookie,
-  getRawCookies,
-  removeClientCookie,
-} from '@camp/infra';
+import { getClientCookie, removeClientCookie } from '@camp/infra';
 
 export const isAuthVar = makeVar(false);
-await getRawCookies().then(res => console.log(res));
 
 export const loginLocally = async () => {
   await getClientCookie()
@@ -18,7 +13,7 @@ export const loginLocally = async () => {
     .catch(err => console.log(err));
 };
 
-export const logoutLocally = async () => {
-  await removeClientCookie();
+export const logoutLocally = () => {
+  void removeClientCookie();
   isAuthVar(false);
 };
