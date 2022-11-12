581231913d4c693e3aa22588da74fb796a0a52a8
mojtaba eslami <mojtabaeslami.72@gmail.com>
2022-11-12 16:41:57 +0330

    Extract custom styles from ExitNavLink

--- src/components/atoms/NavLink/ExitNavLink.tsx
+++ src/components/atoms/NavLink/ExitNavLink.tsx
@@ -1,6 +1,7 @@
 import { ExitIcon } from '@camp/design';
 import { messages } from '@camp/messages';
 import { createTestAttr } from '@camp/utils';
+import type { CSSObject, Sx } from '@mantine/core';
 import { NavLink as MantineNavLink } from '@mantine/core';
 
 import { exitNavLinkId as id } from './ExitNavLink.ids';
@@ -10,16 +11,18 @@ interface ExitNavLinkProps {
   onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
+const rootStyles: Exclude<Sx, CSSObject> = theme => ({
+  ...navLinkRootStyles(theme),
+  '&:hover': {
+    backgroundColor: theme.colors.red[0],
+    color: theme.colors.red[6],
+  },
+});
+
 export const ExitNavLink = ({ onClick }: ExitNavLinkProps) => {
   return (
     <MantineNavLink
-      sx={theme => ({
-        ...navLinkRootStyles(theme),
-        '&:hover': {
-          backgroundColor: theme.colors.red[0],
-          color: theme.colors.red[6],
-        },
-      })}
+      sx={rootStyles}
       label={messages.logout.link}
       rightSection={<ExitIcon />}
       onClick={onClick}
