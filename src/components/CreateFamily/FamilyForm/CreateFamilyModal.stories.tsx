5c2f27ea1d8618e418f331085f7b6b4436c78095
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-11-14 12:46:49 +0330

    fix storybook createFamilyModal

--- src/components/CreateFamily/FamilyForm/CreateFamilyModal.stories.tsx
+++ src/components/CreateFamily/FamilyForm/CreateFamilyModal.stories.tsx
@@ -1,5 +1,7 @@
+import { noop } from '@fullstacksjs/toolbox';
 import type { Meta, Story } from '@storybook/react';
 
+import { CreateFamilyModalStateCtx } from '../../../contexts';
 import { CreateFamilyModal } from './CreateFamilyModal';
 
 export default {
@@ -13,8 +15,15 @@ export default {
   component: CreateFamilyModal,
 } as Meta;
 
-const Template: Story<typeof CreateFamilyModal> = args => {
-  return <CreateFamilyModal {...args} />;
+const Template: Story<typeof CreateFamilyModal> = () => {
+  return (
+    <CreateFamilyModalStateCtx.Provider
+      // eslint-disable-next-line react/jsx-no-constructed-context-values
+      value={{ isModalOpen: true, closeModal: noop, openModal: noop }}
+    >
+      <CreateFamilyModal />;
+    </CreateFamilyModalStateCtx.Provider>
+  );
 };
 
 export const Default = Template.bind({});
