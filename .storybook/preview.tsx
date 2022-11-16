16cb98eddd830ed816d8ce7b7757ba8fa3114372
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-11-16 11:43:16 +0330

    fix GlobalContextProvider in storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NotificationsProvider } from '@mantine/notifications';
 import { CreateProjectDocument } from '../src/data-layer';
-import { ContextProvider } from '../src/contexts';
+import { GlobalContextProvider } from '../src/contexts';
 import { MockedProvider } from '@apollo/client/testing';
 import {
   createMemoryHistory,
@@ -68,9 +68,9 @@ export const decorators: DecoratorFn[] = [
   Story => (
     <ThemeProvider>
       <NotificationsProvider limit={3}>
-        <ContextProvider>
+        <GlobalContextProvider>
           <Story />
-        </ContextProvider>
+        </GlobalContextProvider>
       </NotificationsProvider>
     </ThemeProvider>
   ),
