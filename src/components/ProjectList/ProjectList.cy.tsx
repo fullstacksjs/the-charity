a4ed36b6ba0178861672387a5ff00db7d46be0bb
erfan-goodarzi <devgoodarzi@protonmail.com>
2022-11-30 18:12:08 +0330

    fix ProjectList component test issue

--- src/components/ProjectList/ProjectList.cy.tsx
+++ src/components/ProjectList/ProjectList.cy.tsx
@@ -1,12 +1,13 @@
-import { createProjectButtonId, createProjectModalId } from '../CreateProject';
+import { createProjectButtonId } from '../CreateProject/ProjectButton/CreateProjectButton.ids';
+import { createProjectModalId } from '../CreateProject/ProjectForm/CreateProjectModal.ids';
 import { ProjectList } from './ProjectList';
 
 describe('Project List', () => {
   beforeEach(() => {
     cy.mount(<ProjectList />);
   });
-  // This test works but after https://github.com/fullstacksjs/the-charity-frontend/pull/62 got merged
-  it.skip('should close their menu on second click', () => {
+
+  it('should close their menu on second click', () => {
     cy.findByTestId(createProjectButtonId).click();
     cy.findByTestId(createProjectModalId).should('exist');
   });
