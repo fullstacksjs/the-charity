2d56572aa5231f228a007a0f0bfe5750445e414a
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-12-24 16:39:47 +0330

    add fake shortFamiliesInfo

--- app/components/FamilyList/FamilyTable/FamilyTable.stories.tsx
+++ app/components/FamilyList/FamilyTable/FamilyTable.stories.tsx
@@ -1,7 +1,29 @@
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
 
-import { shortFamiliesInfo } from '../FamilyList';
+import { FamilySeverityEnum, FamilyStatusEnum } from '../../../data-layer';
 import { FamilyTable } from './FamilyTable';
+import type { ShortFamiliesInfo } from './toShortFamilyInfoTableRows';
+
+export const shortFamiliesInfo: ShortFamiliesInfo = [
+  {
+    id: '(F00001)',
+    name: 'فول استک زاده',
+    severity: FamilySeverityEnum.Critical,
+    status: FamilyStatusEnum.Completed,
+  },
+  {
+    id: '(F00002)',
+    name: 'فول استک زاده',
+    severity: FamilySeverityEnum.Critical,
+    status: FamilyStatusEnum.Draft,
+  },
+  {
+    id: '(F00003)',
+    name: 'فول استک زاده',
+    severity: FamilySeverityEnum.Normal,
+    status: FamilyStatusEnum.Completed,
+  },
+];
 
 export default {
   component: FamilyTable,
