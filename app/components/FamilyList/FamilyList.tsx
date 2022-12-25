ca91f5b951cfa73a172b6bc164e686ee43ac774d
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-12-25 22:02:29 +0330

    fix familyList outcome

--- app/components/FamilyList/FamilyList.tsx
+++ app/components/FamilyList/FamilyList.tsx
@@ -1,23 +1,25 @@
-import {
-  FamilySeverityEnum,
-  FamilyStatusEnum,
-  useFamilyListQuery,
-} from '@camp/data-layer';
-import { DashboardCard } from '@camp/design';
-import { messages } from '@camp/messages';
+import { useFamilyListQuery } from '@camp/data-layer';
+import { DashboardCard, FullPageLoader, showNotification } from '@camp/design';
+import { errorMessages, messages } from '@camp/messages';
+import { isEmpty, isNull } from '@fullstacksjs/toolbox';
 import { Title } from '@mantine/core';
 
 import { CreateFamilyButton } from '../CreateFamily';
+import { FamilyEmptyState } from '../FamilyEmptyState';
 import { FamilyTable } from './FamilyTable';
-import type { ShortFamilyInfo } from './FamilyTable/toShortFamilyInfoTableRows';
 
 export const FamilyList = () => {
   const t = messages.families.list;
   const { data, loading, error } = useFamilyListQuery();
+  const families = data?.family;
 
-  if (error) return <>error</>;
-  if (loading) return <>Loading</>;
+  if (error ?? isNull(families)) {
+    showNotification({ type: 'failure', message: errorMessages.UNKNOWN_ERROR });
+    return null;
+  }
+  if (loading) return <FullPageLoader />;
 
+  if (isEmpty(families)) return <FamilyEmptyState />;
   return (
     <DashboardCard
       left={<CreateFamilyButton />}
@@ -27,7 +29,7 @@ export const FamilyList = () => {
         </Title>
       }
     >
-      <FamilyTable shortFamiliesInfo={data?.family as ShortFamilyInfo[]} />
+      <FamilyTable shortFamiliesInfo={families} />
     </DashboardCard>
   );
 };
