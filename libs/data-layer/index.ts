f8bae137c50b5963c5f4f1462e80aac4325144ea
AmirabbasJ <amirabbasgamer2@gmail.com>
2023-01-06 14:42:49 +0330

    rename graphql generated types

--- libs/data-layer/index.ts
+++ libs/data-layer/index.ts
@@ -3,16 +3,13 @@ export type {
   Family,
   FamilyListQuery,
   FamilyQuery,
-} from './operations/__generated__/typesAndHooks';
-export type {
-  Project,
-  ProjectListQuery,
-} from './operations/__generated__/typesAndHooks';
+} from './operations/__generated__/api';
+export type { Project, ProjectListQuery } from './operations/__generated__/api';
 export {
   FamilySeverityEnum,
   FamilyStatusEnum,
   ProjectStatusEnum,
-} from './operations/__generated__/typesAndHooks';
+} from './operations/__generated__/api';
 export {
   CreateProjectDocument,
   FamilyDocument,
@@ -21,7 +18,7 @@ export {
   useCreateProjectMutation,
   useFamilyListQuery,
   useFamilyQuery,
-} from './operations/__generated__/typesAndHooks';
-export { ProjectListDocument } from './operations/__generated__/typesAndHooks';
-export { useProjectListQuery } from './operations/__generated__/typesAndHooks';
+} from './operations/__generated__/api';
+export { ProjectListDocument } from './operations/__generated__/api';
+export { useProjectListQuery } from './operations/__generated__/api';
 export { useLoginMutation } from './operations/mutations/useLoginMutation';
