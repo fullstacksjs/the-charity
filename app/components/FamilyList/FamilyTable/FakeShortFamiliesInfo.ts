e5e7c689a8ccae5da874191e6c480956acb95889
AmirabbasJ <amirabbasgamer2@gmail.com>
2023-01-06 16:53:32 +0330

    add fake data for Family Table test and story

---
+++ app/components/FamilyList/FamilyTable/FakeShortFamiliesInfo.ts
@@ -0,0 +1,24 @@
+import { FamilySeverityEnum, FamilyStatusEnum } from '@camp/data-layer';
+
+import type { ShortFamiliesInfo } from './FamilyTable/toShortFamilyInfoTableRows';
+
+export const shortFamiliesInfo: ShortFamiliesInfo = [
+  {
+    id: '(F00001)',
+    name: 'فول استک زاده',
+    severity: FamilySeverityEnum.Critical,
+    status: FamilyStatusEnum.Completed,
+  },
+  {
+    id: '(F00002)',
+    name: 'فول استک زاده',
+    severity: FamilySeverityEnum.Critical,
+    status: FamilyStatusEnum.Draft,
+  },
+  {
+    id: '(F00003)',
+    name: 'فول استک زاده',
+    severity: FamilySeverityEnum.Normal,
+    status: FamilyStatusEnum.Completed,
+  },
+];
