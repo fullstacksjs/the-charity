36c03bce09c24819ce78ddf5163a1d711d3833f2
Alireza Safaierad <frontendmonster@gmail.com>
2023-02-12 21:07:24 +0400

    fix create family

--- cypress/e2e/createFamily.cy.ts
+++ cypress/e2e/createFamily.cy.ts
@@ -1,45 +1,26 @@
 import { createFamilyButtonId } from '../../app/components/CreateFamily/FamilyButton/CreateFamilyButton.ids';
 import { createFamilyFormIds } from '../../app/components/CreateFamily/FamilyForm/CreateFamilyForm.ids';
-import { createFamilyModalId } from '../../app/components/CreateFamily/FamilyForm/CreateFamilyModal.ids';
 import { dashboardHeaderId } from '../../libs/design/Header/DashboardHeader.ids';
-import { navLinkIds } from '../../libs/design/Sidebar/Sidebar.ids';
 import { AppRoute } from '../../libs/router/AppRoutes';
 import { admin } from '../fixtures/admin';
 import { familyFixture } from '../fixtures/project';
-
-const createFamily = (name: string) => {
-  cy.visit(AppRoute.families);
-  cy.findByTestId(dashboardHeaderId).findByTestId(createFamilyButtonId).click();
-  cy.findByTestId(createFamilyFormIds.form).within(() => {
-    cy.findByTestId(createFamilyFormIds.nameInput).find('input').type(name);
-    cy.findByTestId(createFamilyFormIds.submitBtn).click();
-  });
-  cy.findByTestId(createFamilyFormIds.notification.success, {
-    timeout: 1e4,
-  }).should('exist');
-};
 
 describe('Create Family', () => {
   beforeEach(() => {
     cy.login(admin);
     cy.visit(AppRoute.families);
-    cy.findByTestId(navLinkIds.families).click();
+  });
+
+  it('[OK]: Create a family', () => {
     cy.findByTestId(dashboardHeaderId)
       .findByTestId(createFamilyButtonId)
       .click();
-  });
-
-  it('should open modal', () => {
-    cy.findByTestId(createFamilyModalId).should('exist');
-  });
-
-  it('should close modal after success', () => {
-    createFamily(familyFixture.name());
-    cy.findByTestId(createFamilyModalId, { timeout: 1e4 }).should('not.exist');
-  });
-
-  it('should show result after success', () => {
-    createFamily(familyFixture.name());
+    cy.findByTestId(createFamilyFormIds.form).within(() => {
+      cy.findByTestId(createFamilyFormIds.nameInput)
+        .find('input')
+        .type(familyFixture.name());
+      cy.findByTestId(createFamilyFormIds.submitBtn).click();
+    });
     cy.findByTestId(createFamilyFormIds.notification.success, {
       timeout: 1e4,
     }).should('exist');
