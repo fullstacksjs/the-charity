338d8c70ee49a10d307789e633c4f615f3c91a06
AmirabbasJ <amirabbasgamer2@gmail.com>
2023-02-19 20:52:57 +0330

    fix action bar eslint error

--- libs/design/ActionButton/ActionButton.cy.tsx
+++ libs/design/ActionButton/ActionButton.cy.tsx
@@ -12,14 +12,12 @@ describe('ActionButton', () => {
 
   describe('Action Buttons', () => {
     it('should open their menu on first click', () => {
-      const button = cy.findAllByTestId(menuButtonId);
-      button.click();
+      cy.findAllByTestId(menuButtonId).click();
       cy.findByTestId(menuId).should('be.visible');
     });
 
     it('should close their menu on second click', () => {
-      const button = cy.findAllByTestId(menuButtonId);
-      button.click().click();
+      cy.findAllByTestId(menuButtonId).click().click();
       cy.findByTestId(menuId).should('be.hidden');
     });
   });
