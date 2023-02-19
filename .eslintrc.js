e44394c78ab19ec73361471af18c61eae2917faa
Alireza Safaierad <frontendmonster@gmail.com>
2023-02-19 22:53:36 +0400

    [FS-197]: Cycle 9 Third-Party Maintenance (#81)
    This PR cycles 9 third-party maintenance.
    
    ---------
    
    Co-authored-by: AmirabbasJ <amirabbasgamer2@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+const { init } = require('@fullstacksjs/eslint-config/init');
+
+module.exports = init({
+  modules: {
+    auto: true,
+    esm: true,
+    typescript: {
+      parserProject: ['./tsconfig.eslint.json'],
+      resolverProject: ['tsconfig.json'],
+    },
+  },
+  root: true,
+  rules: {
+    // NOTE: https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/no-unused-prop-types.md#known-issueslimitations
+    'react/no-unused-prop-types': 'off',
+  },
+  overrides: [
+    {
+      files: ['./cypress/**/*.ts'],
+      rules: {
+        'node/prefer-global/buffer': 'off',
+      },
+    },
+    {
+      files: ['./**/*.stories.tsx', './**/*.cy.tsx'],
+      rules: {
+        '@typescript-eslint/no-confusing-void-expression': ['off'],
+      },
+    },
+  ],
+});
