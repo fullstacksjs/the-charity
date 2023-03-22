803e7e8bf8bdb6099582a72cd8ecbd5f6c801ea7
AmirabbasJ <amirabbasgamer2@gmail.com>
2023-03-22 21:00:22 +0330

    show page loader on loading

--- app/components/HouseholderForm/HouseholderIdentityForm.tsx
+++ app/components/HouseholderForm/HouseholderIdentityForm.tsx
@@ -1,12 +1,12 @@
 import 'dayjs/locale/fa';
 
 import { useHouseholderQuery, useUpsertHouseholder } from '@camp/data-layer';
-import { showNotification } from '@camp/design';
-import type { Gender } from '@camp/domain';
+import { FullPageLoader, showNotification } from '@camp/design';
 import {
   cities,
   countries,
   createResolver,
+  type Gender,
   genders,
   householderIdentitySchema,
   religions,
@@ -96,7 +96,7 @@ export const HouseholderIdentityForm = ({ familyId }: Props) => {
   useEffect(() => {
     if (data != null) reset(data.householder);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [loading, reset]);
+  }, [loading]);
 
   const onSubmit = handleSubmit(formData => {
     upsertHouseholder({
@@ -123,7 +123,7 @@ export const HouseholderIdentityForm = ({ familyId }: Props) => {
   return (
     <form onSubmit={onSubmit} {...createTestAttr(ids.form)}>
       <Stack spacing={25}>
-        <Group position="apart">
+        <Group position="apart" mih="100%">
           <Title order={4} color="fgMuted" weight="bold">
             {t.title}
           </Title>
@@ -137,142 +137,151 @@ export const HouseholderIdentityForm = ({ familyId }: Props) => {
             {t.submitBtn}
           </Button>
         </Group>
-        <SimpleGrid cols={3} spacing="lg" verticalSpacing={20}>
-          <TextInput
-            wrapperProps={createTestAttr(ids.firstNameInput)}
-            {...register('name')}
-            className={classes.textInput}
-            label={`${t.nameInput.label}:`}
-            placeholder={t.nameInput.placeholder}
-            error={errors.name?.message}
-          />
-          <TextInput
-            wrapperProps={createTestAttr(ids.lastNameInput)}
-            {...register('surname')}
-            className={classes.textInput}
-            label={`${t.lastNameInput.label}:`}
-            error={errors.surname?.message}
-            placeholder={t.lastNameInput.placeholder}
-          />
-          <TextInput
-            wrapperProps={createTestAttr(ids.fatherNameInput)}
-            {...register('fatherName')}
-            className={classes.textInput}
-            label={`${t.fatherNameInput.label}:`}
-            placeholder={t.fatherNameInput.placeholder}
-            error={errors.fatherName?.message}
-          />
-          <Controller
-            name="nationality"
-            control={control}
-            render={({ field }) => (
-              <Select
-                wrapperProps={createTestAttr(ids.nationalityInput)}
-                data={countries.map(v => ({
-                  value: v,
-                  label: t.nationalityInput.options[v],
-                }))}
-                placeholder={t.selectInputs.placeholder}
-                label={`${t.nationalityInput.label}:`}
-                error={errors.nationality?.message}
-                {...field}
-              />
-            )}
-          />
-          <TextInput
-            wrapperProps={createTestAttr(ids.nationalIdInput)}
-            error={errors.nationalId?.message}
-            className={classes.textInput}
-            {...register('nationalId')}
-            placeholder={t.nationalIdInput.placeholder}
-            label={`${t.nationalIdInput.label}:`}
-          />
+        {loading ? (
+          <FullPageLoader />
+        ) : (
+          <SimpleGrid cols={3} spacing="lg" verticalSpacing={20}>
+            <TextInput
+              wrapperProps={createTestAttr(ids.firstNameInput)}
+              {...register('name')}
+              className={classes.textInput}
+              label={`${t.nameInput.label}:`}
+              placeholder={t.nameInput.placeholder}
+              error={errors.name?.message}
+            />
+            <TextInput
+              wrapperProps={createTestAttr(ids.lastNameInput)}
+              {...register('surname')}
+              className={classes.textInput}
+              label={`${t.lastNameInput.label}:`}
+              error={errors.surname?.message}
+              placeholder={t.lastNameInput.placeholder}
+            />
+            <TextInput
+              wrapperProps={createTestAttr(ids.fatherNameInput)}
+              {...register('fatherName')}
+              className={classes.textInput}
+              label={`${t.fatherNameInput.label}:`}
+              placeholder={t.fatherNameInput.placeholder}
+              error={errors.fatherName?.message}
+            />
+            <Controller
+              name="nationality"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  clearable
+                  wrapperProps={createTestAttr(ids.nationalityInput)}
+                  data={countries.map(v => ({
+                    value: v,
+                    label: t.nationalityInput.options[v],
+                  }))}
+                  placeholder={t.selectInputs.placeholder}
+                  label={`${t.nationalityInput.label}:`}
+                  error={errors.nationality?.message}
+                  {...field}
+                />
+              )}
+            />
+            <TextInput
+              wrapperProps={createTestAttr(ids.nationalIdInput)}
+              error={errors.nationalId?.message}
+              className={classes.textInput}
+              {...register('nationalId')}
+              placeholder={t.nationalIdInput.placeholder}
+              label={`${t.nationalIdInput.label}:`}
+            />
 
-          <Controller
-            name="gender"
-            control={control}
-            render={({ field }) => (
-              <Select
-                wrapperProps={createTestAttr(ids.genderInput)}
-                data={genders.map(v => ({
-                  value: v,
-                  label: t.genderInput.options[v],
-                }))}
-                label={`${t.genderInput.label}:`}
-                placeholder={t.selectInputs.placeholder}
-                error={errors.gender?.message}
-                {...field}
-              />
-            )}
-          />
+            <Controller
+              name="gender"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  clearable
+                  wrapperProps={createTestAttr(ids.genderInput)}
+                  data={genders.map(v => ({
+                    value: v,
+                    label: t.genderInput.options[v],
+                  }))}
+                  label={`${t.genderInput.label}:`}
+                  placeholder={t.selectInputs.placeholder}
+                  error={errors.gender?.message}
+                  {...field}
+                />
+              )}
+            />
 
-          <Controller
-            name="issuedAt"
-            control={control}
-            render={({ field }) => (
-              <Select
-                wrapperProps={createTestAttr(ids.issuedAtInput)}
-                data={cities.map(v => ({
-                  value: v,
-                  label: t.issuedAtInput.options[v],
-                }))}
-                placeholder={t.selectInputs.placeholder}
-                label={`${t.issuedAtInput.label}:`}
-                error={errors.issuedAt?.message}
-                {...field}
-              />
-            )}
-          />
+            <Controller
+              name="issuedAt"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  clearable
+                  wrapperProps={createTestAttr(ids.issuedAtInput)}
+                  data={cities.map(v => ({
+                    value: v,
+                    label: t.issuedAtInput.options[v],
+                  }))}
+                  placeholder={t.selectInputs.placeholder}
+                  label={`${t.issuedAtInput.label}:`}
+                  error={errors.issuedAt?.message}
+                  {...field}
+                />
+              )}
+            />
 
-          <Controller
-            name="religion"
-            control={control}
-            render={({ field }) => (
-              <Select
-                wrapperProps={createTestAttr(ids.religionInput)}
-                data={religions.map(v => ({
-                  value: v,
-                  label: t.religionInput.options[v],
-                }))}
-                placeholder={t.selectInputs.placeholder}
-                label={`${t.religionInput.label}:`}
-                error={errors.religion?.message}
-                {...field}
-              />
-            )}
-          />
+            <Controller
+              name="religion"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  clearable
+                  wrapperProps={createTestAttr(ids.religionInput)}
+                  data={religions.map(v => ({
+                    value: v,
+                    label: t.religionInput.options[v],
+                  }))}
+                  placeholder={t.selectInputs.placeholder}
+                  label={`${t.religionInput.label}:`}
+                  error={errors.religion?.message}
+                  {...field}
+                />
+              )}
+            />
 
-          <Controller
-            name="cityOfBirth"
-            control={control}
-            render={({ field }) => (
-              <Select
-                wrapperProps={createTestAttr(ids.cityOfBirthInput)}
-                data={cities.map(v => ({
-                  value: v,
-                  label: t.cityOfBirthInput.options[v],
-                }))}
-                placeholder={t.selectInputs.placeholder}
-                label={`${t.cityOfBirthInput.label}:`}
-                error={errors.cityOfBirth?.message}
-                {...field}
-              />
-            )}
-          />
+            <Controller
+              name="cityOfBirth"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  clearable
+                  wrapperProps={createTestAttr(ids.cityOfBirthInput)}
+                  data={cities.map(v => ({
+                    value: v,
+                    label: t.cityOfBirthInput.options[v],
+                  }))}
+                  placeholder={t.selectInputs.placeholder}
+                  label={`${t.cityOfBirthInput.label}:`}
+                  error={errors.cityOfBirth?.message}
+                  {...field}
+                />
+              )}
+            />
 
-          <DateInput
-            wrapperProps={createTestAttr(ids.dateOfBirthInput)}
-            className={classes.dateInput}
-            rightSection={<CalendarIcon stroke="currentColor" size={16} />}
-            label={`${t.dateOfBirthInput.label}:`}
-            sx={theme => ({
-              direction: 'ltr',
-              color: theme.colors.secondaryDefault[6],
-            })}
-            locale="fa"
-            placeholder={t.selectInputs.placeholder}
-          />
-        </SimpleGrid>
+            <DateInput
+              wrapperProps={createTestAttr(ids.dateOfBirthInput)}
+              className={classes.dateInput}
+              rightSection={<CalendarIcon stroke="currentColor" size={16} />}
+              label={`${t.dateOfBirthInput.label}:`}
+              sx={theme => ({
+                direction: 'ltr',
+                color: theme.colors.secondaryDefault[6],
+              })}
+              locale="fa"
+              placeholder={t.selectInputs.placeholder}
+            />
+          </SimpleGrid>
+        )}{' '}
       </Stack>
     </form>
   );
