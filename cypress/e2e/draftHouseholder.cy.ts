c417a0758084e21914e92cd3dbb85e622726d717
AmirabbasJ <amirabbasgamer2@gmail.com>
2023-03-31 14:42:06 +0330

    refactor draftHouseholder e2e

--- cypress/e2e/draftHouseholder.cy.ts
+++ cypress/e2e/draftHouseholder.cy.ts
@@ -11,7 +11,6 @@ import * as api from './api';
 
 interface TextInput {
   id: string;
-
   type: 'text';
 }
 
@@ -21,7 +20,14 @@ interface SelectInput {
   options: Record<string, string>;
 }
 
-type Input = SelectInput | TextInput;
+interface DateInput {
+  type: 'date';
+  id: string;
+  format: (d: Date) => string;
+}
+//
+
+type Input = DateInput | SelectInput | TextInput;
 
 const idMapping = {
   name: { id: ids.firstNameInput, type: 'text' },
@@ -39,7 +45,12 @@ const idMapping = {
     type: 'select',
     options: messages.religions,
   },
-  dob: { id: ids.dobInput, type: 'text' },
+  dob: {
+    id: ids.dobInput,
+    type: 'date',
+    format: (d: Date) =>
+      new Intl.DateTimeFormat('fa-IR', { dateStyle: 'long' } as any).format(d),
+  },
   city: { id: ids.cityOfBirthInput, type: 'select', options: messages.cities },
   issuedAt: { id: ids.issuedAtInput, type: 'select', options: messages.cities },
 } as Record<string, Input>;
@@ -55,28 +66,8 @@ describe('Householder', () => {
   });
 
   it('[OK]: Admin wants to add householder information', () => {
-    cy.findByTestId(ids.form).within(() => {
-      const mock = householderFixture();
-      Object.keys(mock).forEach(key => {
-        const input = idMapping[key];
-        const mockValue = mock[key];
-
-        const { id, type } = input;
-        const inputValue =
-          type === 'text' ? mockValue : input.options[mockValue];
-
-        if (inputValue == null) return;
-        if (type === 'text') cy.findByTestId(id).type(inputValue);
-        if (type === 'select')
-          cy.findByTestId(id).click().findByText(inputValue).click();
-      });
-
-      cy.root().submit();
-    });
-
-    cy.findByTestId(ids.notification.success, {
-      timeout: 1e4,
-    }).should('exist');
+    const mock = pruneUndefinedOrEmpty(householderFixture());
+    addHouseholder(mock);
   });
 
   it('[NOK]: Admin wants to add householder without specifying a name', () => {
@@ -88,108 +79,82 @@ describe('Householder', () => {
   it('[OK]: Admin wants to undo changes after editing form', () => {
     const mock = pruneUndefinedOrEmpty(householderFixture());
 
-    cy.findByTestId(ids.form).within(() => {
-      Object.keys(mock).forEach(key => {
-        const input = idMapping[key];
-        const mockValue = mock[key];
-
-        const { id, type } = input;
-        const inputValue =
-          type === 'text' ? mockValue : input.options[mockValue];
-
-        if (inputValue == null) return;
-        if (type === 'text')
-          cy.findByTestId(id).find('input').type(inputValue).blur();
-        if (type === 'select')
-          cy.findByTestId(id).click('bottom').findByText(inputValue).click();
-      });
-
-      cy.root().submit();
-    });
-
-    cy.findByTestId(ids.notification.success, {
-      timeout: 1e4,
-    }).should('exist');
-
-    Object.keys(mock).forEach(key => {
-      const input = idMapping[key];
-      const mockValue = mock[key];
-      const { id, type } = input;
-      const inputValue = type === 'text' ? mockValue : input.options[mockValue];
-
-      if (inputValue == null) return;
-      if (type === 'text') cy.findByTestId(id).find('input').clear().blur();
-      // NOTE: clicking on the same selected element unselects it
-      else if (type === 'select')
-        cy.findByTestId(id).click().findByText(inputValue).click();
-    });
+    addHouseholder(mock);
+    emptyHouseholderForm(mock);
     cy.findByTestId(ids.undoBtn).click();
-
-    Object.keys(mock).forEach(key => {
-      const input = idMapping[key];
-      const mockValue = mock[key];
-
-      const { id, type } = input;
-      const inputValue = type === 'text' ? mockValue : input.options[mockValue];
-
-      if (inputValue == null) return;
-      // FIXME
-      if (id === ids.dobInput)
-        cy.findByTestId(id).find('input').should('not.have.value', '');
-      else if (type === 'text')
-        cy.findByTestId(id).find('input').should('have.value', inputValue);
-      else if (type === 'select')
-        cy.findByTestId(id)
-          .find('input[type="search"]')
-          .should('have.value', inputValue);
-    });
+    compareHouseholderForm(mock);
   });
 
   it('[OK]: Admin wants to revisit householder information', () => {
     const mock = pruneUndefinedOrEmpty(householderFixture());
-
-    cy.findByTestId(ids.form).within(() => {
-      Object.keys(mock).forEach(key => {
-        const input = idMapping[key];
-        const mockValue = mock[key];
-
-        const { id, type } = input;
-        const inputValue =
-          type === 'text' ? mockValue : input.options[mockValue];
-
-        if (inputValue == null) return;
-        if (type === 'text')
-          cy.findByTestId(id).find('input').type(inputValue).blur();
-        if (type === 'select')
-          cy.findByTestId(id).click('bottom').findByText(inputValue).click();
-      });
-
-      cy.root().submit();
-    });
-
-    cy.findByTestId(ids.notification.success, {
-      timeout: 1e4,
-    }).should('exist');
-
+    addHouseholder(mock);
     cy.reload();
+    compareHouseholderForm(mock);
+  });
+});
 
+type Mock = ReturnType<typeof householderFixture>;
+
+function emptyHouseholderForm(mock: Mock) {
+  Object.keys(mock).forEach(key => {
+    const input = idMapping[key];
+    const mockValue = mock[key];
+    const { id, type } = input;
+    const inputValue = type === 'select' ? input.options[mockValue] : mockValue;
+
+    if (inputValue == null) return;
+    if (type === 'text' || type === 'date')
+      cy.findByTestId(id).find('input').clear().blur();
+    // NOTE: clicking on the same selected element unselects it
+    if (type === 'select')
+      cy.findByTestId(id).click('bottom').findByText(inputValue).click();
+  });
+}
+
+function addHouseholder(mock: Mock) {
+  cy.findByTestId(ids.form).within(() => {
     Object.keys(mock).forEach(key => {
       const input = idMapping[key];
       const mockValue = mock[key];
 
       const { id, type } = input;
-      const inputValue = type === 'text' ? mockValue : input.options[mockValue];
+      const inputValue =
+        type === 'select' ? input.options[mockValue] : mockValue;
 
       if (inputValue == null) return;
-      // FIXME
-      if (id === ids.dobInput)
-        cy.findByTestId(id).find('input').should('not.have.value', '');
-      else if (type === 'text')
-        cy.findByTestId(id).find('input').should('have.value', inputValue);
-      else if (type === 'select')
-        cy.findByTestId(id)
-          .find('input[type="search"]')
-          .should('have.value', inputValue);
+      if (type === 'text' || type === 'date')
+        cy.findByTestId(id).find('input').type(inputValue).blur();
+      if (type === 'select')
+        cy.findByTestId(id).click('bottom').findByText(inputValue).click();
     });
+
+    cy.root().submit();
   });
-});
+
+  cy.findByTestId(ids.notification.success, {
+    timeout: 1e4,
+  }).should('exist');
+
+  return mock;
+}
+
+function compareHouseholderForm(mock: Mock) {
+  Object.keys(mock).forEach(key => {
+    const input = idMapping[key];
+    const mockValue = mock[key];
+
+    const { id, type } = input;
+    const inputValue = type === 'select' ? input.options[mockValue] : mockValue;
+
+    if (inputValue == null) return;
+    // FIXME
+    if (id === ids.dobInput)
+      cy.findByTestId(id).find('input').should('not.have.value', '');
+    else if (type === 'text')
+      cy.findByTestId(id).find('input').should('have.value', inputValue);
+    else if (type === 'select')
+      cy.findByTestId(id)
+        .find('input[type="search"]')
+        .should('have.value', inputValue);
+  });
+}
