14f4e7ce8a07b39e41d5f417238d12261003f642
erfan-goodarzi <devgoodarzi@protonmail.com>
2023-05-05 22:09:06 +0330

    fix delete family modal naming issue

--- app/components/DeleteFamilyModal/DeleteFamilyModal.tsx
+++ app/components/DeleteFamilyModal/DeleteFamilyModal.tsx
@@ -5,15 +5,12 @@ import { deleteFamilyModalIds as Ids } from './DeleteFamilyModal.ids';
 
 const t = messages.families.list.delete.modal;
 
-interface DeleteFamilyModalProps {
+interface Props {
   name: string;
-  onDeleteFamily: () => void;
+  onDeleteFamily: VoidFunction;
 }
 
-export const openDeleteFamilyModal = ({
-  name,
-  onDeleteFamily,
-}: DeleteFamilyModalProps) =>
+export const openDeleteFamilyModal = ({ name, onDeleteFamily }: Props) =>
   showModal({
     id: Ids.modal,
     title: t.title,
