212f92ed88216be66172f49c860490867e06caaf
Alireza Safaierad <frontendmonster@gmail.com>
2023-05-08 11:50:56 +0400

    add missing tests

--- libs/debug/Debug.spec.ts
+++ libs/debug/Debug.spec.ts
@@ -18,6 +18,69 @@ describe('Debug', () => {
     };
   });
 
+  describe('trace', () => {
+    it('it should call internal log function when log level is trace', () => {
+      const debug = createDebug('trace');
+      debug.trace('Test', 1, 2);
+
+      expect(stubLogger.log).toBeCalledWith('%cTest', style, 1, 2);
+    });
+
+    it('it should NOT call internal log function when log level is info', () => {
+      const debug = createDebug('info');
+      debug.trace('Test', 1, 2);
+
+      expect(stubLogger.log).not.toBeCalled();
+    });
+
+    it('it should NOT call internal log function when log level is warn', () => {
+      const debug = createDebug('warn');
+      debug.trace('Test', 1, 2);
+
+      expect(stubLogger.log).not.toBeCalled();
+    });
+
+    it('it should NOT call internal log function when log level is error', () => {
+      const debug = createDebug('error');
+      debug.trace('Test', 1, 2);
+
+      expect(stubLogger.log).not.toBeCalled();
+    });
+
+    it('it should NOT call internal log function when log level is silent', () => {
+      const debug = createDebug('silent');
+      debug.trace('Test', 1, 2);
+
+      expect(stubLogger.log).not.toBeCalled();
+    });
+
+    it('it should call internal log function when scope is included', () => {
+      const debug = createDebug('trace', ['Test2', 'Test3']);
+      debug.trace('Test2', 1, 2);
+
+      expect(stubLogger.log).toBeCalledWith('%cTest2', style, 1, 2);
+    });
+
+    it('it should NOT call internal log function when scope is not included', () => {
+      const debug = createDebug('trace', ['Test', 'Test3']);
+      debug.trace('Test2', 1, 2);
+
+      expect(stubLogger.log).not.toBeCalled();
+    });
+
+    it('it should call internal log function when the scope is All even if it is not included', () => {
+      const debug = createDebug('trace', ['Test', 'Test3']);
+      debug.trace(DebugScopes.All, 1, 2);
+
+      expect(stubLogger.log).toBeCalledWith(
+        `%c${DebugScopes.All}`,
+        style,
+        1,
+        2,
+      );
+    });
+  });
+
   describe('log', () => {
     it('it should call internal log function when log level is trace', () => {
       const debug = createDebug('trace');
@@ -81,66 +144,77 @@ describe('Debug', () => {
     });
   });
 
-  describe('trace', () => {
+  describe('warn', () => {
     it('it should call internal log function when log level is trace', () => {
       const debug = createDebug('trace');
-      debug.log('Test', 1, 2);
+      debug.warn(1, 2);
 
-      expect(stubLogger.log).toBeCalledWith('%cTest', style, 1, 2);
+      expect(stubLogger.warn).toBeCalledWith(1, 2);
     });
 
     it('it should NOT call internal log function when log level is info', () => {
       const debug = createDebug('info');
-      debug.log('Test', 1, 2);
+      debug.warn(1, 2);
 
-      expect(stubLogger.log).not.toBeCalled();
+      expect(stubLogger.warn).toBeCalledWith(1, 2);
     });
 
     it('it should NOT call internal log function when log level is warn', () => {
       const debug = createDebug('warn');
-      debug.log('Test', 1, 2);
+      debug.warn(1, 2);
 
-      expect(stubLogger.log).not.toBeCalled();
+      expect(stubLogger.warn).toBeCalledWith(1, 2);
     });
 
     it('it should NOT call internal log function when log level is error', () => {
       const debug = createDebug('error');
-      debug.log('Test', 1, 2);
+      debug.warn(1, 2);
 
-      expect(stubLogger.log).not.toBeCalled();
+      expect(stubLogger.warn).not.toBeCalled();
     });
 
     it('it should NOT call internal log function when log level is silent', () => {
       const debug = createDebug('silent');
-      debug.log('Test', 1, 2);
+      debug.warn(1, 2);
 
-      expect(stubLogger.log).not.toBeCalled();
+      expect(stubLogger.warn).not.toBeCalled();
+    });
+  });
+
+  describe('error', () => {
+    it('it should call internal log function when log level is trace', () => {
+      const debug = createDebug('trace');
+      debug.error(1, 2);
+
+      expect(stubLogger.error).toBeCalledWith(1, 2);
     });
 
-    it('it should call internal log function when scope is included', () => {
-      const debug = createDebug('trace', ['Test2', 'Test3']);
-      debug.log('Test2', 1, 2);
+    it('it should NOT call internal log function when log level is info', () => {
+      const debug = createDebug('info');
+      debug.error(1, 2);
 
-      expect(stubLogger.log).toBeCalledWith('%cTest2', style, 1, 2);
+      expect(stubLogger.error).toBeCalledWith(1, 2);
     });
 
-    it('it should NOT call internal log function when scope is not included', () => {
-      const debug = createDebug('trace', ['Test', 'Test3']);
-      debug.log('Test2', 1, 2);
+    it('it should NOT call internal log function when log level is warn', () => {
+      const debug = createDebug('error');
+      debug.error(1, 2);
 
-      expect(stubLogger.log).not.toBeCalled();
+      expect(stubLogger.error).toBeCalledWith(1, 2);
     });
 
-    it('it should call internal log function when the scope is All even if it is not included', () => {
-      const debug = createDebug('trace', ['Test', 'Test3']);
-      debug.log(DebugScopes.All, 1, 2);
+    it('it should NOT call internal log function when log level is error', () => {
+      const debug = createDebug('error');
+      debug.error(1, 2);
 
-      expect(stubLogger.log).toBeCalledWith(
-        `%c${DebugScopes.All}`,
-        style,
-        1,
-        2,
-      );
+      expect(stubLogger.error).toBeCalledWith(1, 2);
+    });
+
+    it('it should NOT call internal log function when log level is silent', () => {
+      const debug = createDebug('silent');
+      debug.error(1, 2);
+
+      expect(stubLogger.error).not.toBeCalled();
     });
   });
 });
