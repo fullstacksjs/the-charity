c999d7208ead43ad22311521bf4e3a7b0dd34cb6
erfan-goodarzi <devgoodarzi@protonmail.com>
2023-05-10 17:22:52 +0330

    disable select option icon when its readOnly

--- libs/design/TextInput/useReadonlyInputStyles.ts
+++ libs/design/TextInput/useReadonlyInputStyles.ts
@@ -4,11 +4,16 @@ export const useReadonlyInputStyles = createStyles(theme => ({
   readonlyInput: {
     input: {
       ':read-only': {
-        border: 'none',
-        padding: 0,
-        fontSize: theme.fontSizes.md,
-        userSelect: 'text',
-        cursor: 'text',
+        'border': 'none',
+        'padding': 0,
+        'pointerEvents': 'none',
+        'fontSize': theme.fontSizes.md,
+        'userSelect': 'text',
+        'cursor': 'text',
+        '&::placeholder': {
+          // FIXME: it should be removed when the default selectInput option is added.
+          opacity: 0,
+        },
       },
     },
   },
