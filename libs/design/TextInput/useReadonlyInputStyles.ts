7e7d2551be59b6f955dbb7bfe2b335c2a8602ce9
erfan-goodarzi <devgoodarzi@protonmail.com>
2023-05-11 13:39:05 +0330

    hide autofill bg color in readOnly mode

--- libs/design/TextInput/useReadonlyInputStyles.ts
+++ libs/design/TextInput/useReadonlyInputStyles.ts
@@ -10,6 +10,7 @@ export const useReadonlyInputStyles = createStyles(theme => ({
         'fontSize': theme.fontSizes.md,
         'userSelect': 'text',
         'cursor': 'text',
+        'WebkitBoxShadow': '0 0 0px 1000px white inset !important',
         '&::placeholder': {
           // FIXME: it should be removed when the default selectInput option is added.
           opacity: 0,
