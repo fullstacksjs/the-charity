1b9c4b66ffc58e3bef50b528dae26cad14fbc44c
AmirabbasJ <amirabbasgamer2@gmail.com>
2023-05-12 13:37:44 +0330

    add name regex

--- libs/domain/Schema.ts
+++ libs/domain/Schema.ts
@@ -7,25 +7,21 @@ import { Nationality } from './Nationality';
 import { Religion } from './Religions';
 
 export const digitsRegex = /^[0-9]*[\u0660-\u0669\u06F0-\u06F90-9]*$/;
+export const nameRegex =
+  /^[ \u0620-\u063A\u0641-\u064A\u067E\u0686\u0698\u06AF\u06A9\u06CC]+$/;
 
 export const Schema = {
   dob: () => z.date().max(new Date(), { message: messages.validation.dob.max }),
   name: () =>
     z
       .string()
-      .regex(
-        /^[ \u0600-\u06FF\u0750-\u077F\uFB50-\uFBC2\uFBD3-\uFD3D\uFD50-\uFDCF\uFDF0-\uFE4F\uFE70-\uFEFF]+$/,
-        { message: messages.validation.name.invalid },
-      )
+      .regex(nameRegex, { message: messages.validation.name.invalid })
       .min(3, messages.validation.name.minLength)
       .trim(),
   surname: () =>
     z
       .string()
-      .regex(
-        /^[ \u0600-\u06FF\u0750-\u077F\uFB50-\uFBC2\uFBD3-\uFD3D\uFD50-\uFDCF\uFDF0-\uFE4F\uFE70-\uFEFF]+$/,
-        { message: messages.validation.name.invalid },
-      )
+      .regex(nameRegex, { message: messages.validation.name.invalid })
       .min(3, messages.validation.surname.minLength)
       .trim(),
   nationalId: () =>
