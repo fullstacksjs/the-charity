b2efc348aefcb49341e358f70c7d61f1c6007620
AmirabbasJ <amirabbasgamer2@gmail.com>
2023-05-18 23:18:36 +0330

    replace unicode mapping with literal text

--- libs/domain/name.spec.ts
+++ libs/domain/name.spec.ts
@@ -1,20 +1,11 @@
-import { range } from '@fullstacksjs/toolbox';
 import { describe, expect, it } from 'vitest';
 
 import { Schema } from './Schema';
 
-const forbiddenLetters = (
-  [
-    [1536, 1568],
-    [1595, 1600],
-    [1611, 1791],
-  ] as const
-)
-  .flatMap(([a, b]) => range(b - a, { offset: a, step: 1 }))
-  .map(x => String.fromCharCode(x))
-  .join('')
-  .replace(/[\u067E\u0686\u0698\u06AF\u06A9\u06CC]/g, '')
-  .split('');
+const forbiddenLetters =
+  '؀؆؇؈؉؊؋،؍؎؏ؘؙؚؐؑؒؓؔؕؖؗ؛؜؝؞؟ػؼؽؾؿًٌٍَُِّْٕٖٜٟٓٔٗ٘ٙٚٛٝٞ٠١٢٣٤٥٦٧٨٩٪٫٬٭ٮٯٰٱٲٳٴٵٶٷٸٹٺٻټٽٿڀځڂڃڄڅڇڈډڊڋڌڍڎڏڐڑڒړڔڕږڗڙښڛڜڝڞڟڠڡڢڣڤڥڦڧڨڪګڬڭڮڰڱڲڳڴڵڶڷڸڹںڻڼڽھڿۀہۂۃۄۅۆۇۈۉۊۋۍێۏېۑےۓ۔ەۖۗۘۙۚۛۜ۝۞ۣ۟۠ۡۢۤۥۦۧۨ۩۪ۭ۫۬ۮۯ۰۱۲۳۴۵۶۷۸۹ۺۻۼ۽۾'.split(
+    '',
+  );
 
 describe('name', () => {
   it('should pass if name is only persian letters', () => {
