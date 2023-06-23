e63481ba1e6ea97199835a0bcb51318f31c59ec8
erfan-goodarzi <devgoodarzi@protonmail.com>
2023-06-23 20:38:41 +0330

    fix wrong types

--- app/pages/Dashboard/Projects/ProjectBadge/ProjectBadge.tsx
+++ app/pages/Dashboard/Projects/ProjectBadge/ProjectBadge.tsx
@@ -1,15 +1,15 @@
 import { Badge } from '@camp/design';
-import type { ProjectStatus } from '@camp/domain';
+import { ProjectStatusEnum } from '@camp/domain';
 import { messages } from '@camp/messages';
 
 interface Props {
-  status: ProjectStatus;
+  status: ProjectStatusEnum;
 }
 
 const t = messages.projects.informationStatus;
 
 export const ProjectStatusBadge = ({ status }: Props) => {
-  return status === 'done' ? (
+  return status === ProjectStatusEnum.Done ? (
     <Badge status="success">{t.completed}</Badge>
   ) : (
     <Badge status="disabled">{t.disabled}</Badge>
