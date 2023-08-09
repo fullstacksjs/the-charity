b1c82495d3e5813b323d64e675572c8e84c77333
Alireza Safaierad <frontendmonster@gmail.com>
2023-08-10 02:31:20 +0400

    refactor nav link

--- app/Dashboard/_components/ExitNavLink/ExitNavLink.stories.tsx
+++ app/Dashboard/_components/ExitNavLink/ExitNavLink.stories.tsx
@@ -1,16 +1,8 @@
-import { noop } from '@fullstacksjs/toolbox';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { ExitNavLink } from './ExitNavLink';
 
 export default {
-  argTypes: {
-    onClick: {
-      defaultValue: noop,
-      type: 'function',
-      description: 'onClick logic',
-    },
-  },
   component: ExitNavLink,
 } as Meta<typeof ExitNavLink>;
 
--- app/Dashboard/_components/ExitNavLink/ExitNavLink.tsx
+++ app/Dashboard/_components/ExitNavLink/ExitNavLink.tsx
@@ -1,22 +1,12 @@
 import { useAuth0 } from '@camp/auth';
 import { debug } from '@camp/debug';
-import { navLinkRootStyles } from '@camp/design';
+import { NavLink } from '@camp/design';
 import { ExitIcon } from '@camp/icons';
 import { messages } from '@camp/messages';
 import { createTestAttr } from '@camp/test';
-import type { CSSObject, Sx } from '@mantine/core';
-import { NavLink as MantineNavLink } from '@mantine/core';
 
 import { openLogoutModal } from '../LogoutModal';
 import { exitNavLinkId as id } from './ExitNavLink.ids';
-
-const rootStyles: Exclude<Sx, CSSObject> = theme => ({
-  ...navLinkRootStyles(theme),
-  '&:hover': {
-    backgroundColor: theme.colors.red[0],
-    color: theme.colors.red[6],
-  },
-});
 
 export const ExitNavLink = () => {
   const { logout } = useAuth0();
@@ -30,12 +20,13 @@ export const ExitNavLink = () => {
   };
 
   return (
-    <MantineNavLink
-      sx={rootStyles}
+    <NavLink
+      id="logout"
+      variant="destructive"
       label={messages.logout.link}
-      rightSection={<ExitIcon />}
-      {...createTestAttr(id)}
+      icon={<ExitIcon />}
       onClick={handleLogout}
+      {...createTestAttr(id)}
     />
   );
 };
