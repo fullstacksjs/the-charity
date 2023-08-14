swrshho/the-charity-frontend

integrate react table with household list

cypress/e2e/deleteHousehold.cy.ts
@@ -1,4 +1,4 @@
-import { householdRowIds } from '../../app/pages/Dashboard/Households/HouseholdList/HouseholdTableRow.ids';
+import { householdActionIds } from '../../app/pages/Dashboard/Households/HouseholdActionButton/HouseholdActionButton.ids';
 import { AppRoute } from '../../libs/router/AppRoutes';
 import { admin } from '../fixtures/admin';
 import { householdFixture } from '../fixtures/household';
@@ -15,7 +15,7 @@ describe('Delete a Household', () => {
       cy.visit(AppRoute.households);
       cy.findByText(name)
         .parents('tr')
-        .findByTestId(householdRowIds.actionButton)
+        .findByTestId(householdActionIds.actionButton)
         .click();
       cy.findByRole('menuitem', { name: 'حذف' }).click();
       cy.findByRole('button', { name: 'حذف' }).click();

fullstacksjs/the-charity

cypress/e2e/deleteHousehold.cy.ts
@@ -1,8 +1,9 @@
-import { householdRowIds } from '../../app/Dashboard/Households/HouseholdList/HouseholdTableRow.ids';
 import { AppRoute } from '../../libs/router/AppRoutes';
 import { admin } from '../fixtures/admin';
 import { householdFixture } from '../fixtures/household';
 import * as api from './api';
+
+import { householdActionIds } from '../../app/Dashboard/Households/_components/HouseholdActionButton/HouseholdActionButton.ids';
 
 describe('Delete a Household', () => {
   beforeEach(() => {
@@ -15,7 +16,7 @@ describe('Delete a Household', () => {
       cy.visit(AppRoute.households);
       cy.findByText(name)
         .parents('tr')
-        .findByTestId(householdRowIds.actionButton)
+        .findByTestId(householdActionIds.actionButton)
         .click();
       cy.findByRole('menuitem', { name: 'حذف' }).click();
       cy.findByRole('button', { name: 'حذف' }).click();