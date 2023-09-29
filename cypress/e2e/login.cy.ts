c1129d076026b7d1ac836796265f0e33620d6b5e
Alireza Safaierad <frontendmonster@gmail.com>
2023-09-29 16:54:44 +0400

    fix environment variables

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,13 +1,15 @@
 import { AppRoute } from '../../libs/router/AppRoutes';
 import { admin } from '../fixtures/admin';
 
+const domain = Cypress.env('APP_AUTH0_DOMAIN') as string;
+
 describe('Login', () => {
   it('should be redirected to the dashboard page after successful login', () => {
     cy.visit(AppRoute.login);
     cy.get('button').click();
 
     cy.origin(
-      `https://${Cypress.env('APP_AUTH0_DOMAIN') as string}/`,
+      `https://${domain}/`,
       { args: admin } as const,
       ({ username, password }) => {
         cy.get('#username').type(username);
