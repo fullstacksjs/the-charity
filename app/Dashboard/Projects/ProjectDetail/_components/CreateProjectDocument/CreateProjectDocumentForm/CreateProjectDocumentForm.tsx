a6eef082f289a3bf7e590f8db44eceef43ba9618
Alireza Safaierad <frontendmonster@gmail.com>
2024-01-07 17:24:25 +0100

    dead-code

--- app/Dashboard/Projects/ProjectDetail/_components/CreateProjectDocument/CreateProjectDocumentForm/CreateProjectDocumentForm.tsx
+++ app/Dashboard/Projects/ProjectDetail/_components/CreateProjectDocument/CreateProjectDocumentForm/CreateProjectDocumentForm.tsx
@@ -140,7 +140,6 @@ export const CreateProjectDocumentForm = ({ dismiss }: Props) => {
                     .map(r => r.data);
                 }}
               />
-              // </Input.Wrapper>
             );
           }}
         />
