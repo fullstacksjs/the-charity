28f1609713507f413b480117771d09745f8f1354
AmirabbasJ <amirabbasgamer2@gmail.com>
2024-02-27 20:40:07 +0330

    fix mock field

--- cypress/fixtures/householder.ts
+++ cypress/fixtures/householder.ts
@@ -1,7 +1,7 @@
 import { fakerFA } from '@faker-js/faker';
 import { getRandom } from '@fullstacksjs/toolbox';
 
-import { cities, genders, nationalities, religions } from '../../libs/domain';
+import { genders, religions } from '../../libs/domain';
 
 export const householderFixture = () => ({
   name: fakerFA.person.firstName(),
@@ -9,8 +9,6 @@ export const householderFixture = () => ({
   fatherName: fakerFA.person.firstName(),
   nationalId: fakerFA.string.numeric({ length: 10 }),
   gender: getRandom(genders),
-  nationality: getRandom(nationalities),
   religion: getRandom(religions),
-  city: getRandom(cities),
   dob: fakerFA.date.past().toString(),
 });
