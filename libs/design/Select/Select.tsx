d34341228b64ee0a6f4f2b0ba0c8fa47268b66f8
AmirabbasJ <amirabbasgamer2@gmail.com>
2024-02-29 09:01:34 +0330

    fix select label color

--- libs/design/Select/Select.tsx
+++ libs/design/Select/Select.tsx
@@ -1,21 +1,35 @@
 import type { SelectProps as MantineSelectProps } from '@mantine/core';
-import { Select as MantineSelect } from '@mantine/core';
+import { createStyles, Select as MantineSelect } from '@mantine/core';
 import { forwardRef } from 'react';
 
 import { useReadonlyInputStyles } from '../TextInput/useReadonlyInputStyles';
 
 export type SelectProps = MantineSelectProps;
+const useStyles = createStyles(theme => ({
+  select: {
+    label: {
+      color: theme.colors.fg[5],
+    },
+  },
+}));
 
 export const Select = forwardRef<HTMLInputElement, SelectProps>(
   ({ className, readOnly, ...rest }, ref) => {
     const { classes, cx } = useReadonlyInputStyles();
+    const {
+      classes: { select: selectClass },
+    } = useStyles();
 
     return (
       <MantineSelect
         ref={ref}
         readOnly={readOnly}
         {...rest}
-        className={cx({ [classes.readonlyInput]: readOnly }, className)}
+        className={cx(
+          { [classes.readonlyInput]: readOnly },
+          className,
+          selectClass,
+        )}
       />
     );
   },
